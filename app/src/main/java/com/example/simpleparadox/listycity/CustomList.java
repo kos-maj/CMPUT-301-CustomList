07c6ac9ff340394d1905eb8b4ae1b948b22b3c60
unknown <kosmajac@ualberta.ca>
2021-10-31 18:52:00 -0600

    Implemented hasCity() for CustomList

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -58,6 +58,8 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public boolean hasCity(City city) {
+        if(cities.contains(city))
+            return true;
         return false;
     }
 }
